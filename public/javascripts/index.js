1cfc58d2dfd69bca67bf96072d77317909b5c3cb
Sam Lopez <sam.lopez010@gmail.com>
2019-08-23 21:13:56 -0400

    commit

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,7 +8,15 @@ $('.refresh').click(function(){
     socket.emit('refresh');
 })
 
+$('.category').click(function(e){
+    var og = $(e.target).text();
+    var txt = og.toLowerCase();
+
+    socket.emit('category', {category: txt});
+})
+
 socket.on('joke',function(data){
+    console.log(data.data)
     updateText("data",data.data)
 });
 
